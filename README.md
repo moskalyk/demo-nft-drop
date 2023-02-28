1a3a195a8d9ef234a8ce18fd9736abc007626fd5
moskalyk <morgan.moskalyk@protonmail.ch>
2023-02-28 15:32:44 -0500

    initial commit & demo

--- README.md
+++ README.md
@@ -1,46 +1,24 @@
-# Getting Started with Create React App
+# demo-nft-drop
+This is a toy example for how to empower artists to drop ERC1155 NFTs via an interface that enables users to choose between 3 sources of randomization across: time, hex, & space.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## how to run
+```
+$ yarn
+$ yarn start
+```
 
-## Available Scripts
+### randomization
+- time: processor Math.random() && tangent function
+- hex: ethers crypto hex
+- space: fluence p2p round trip
 
-In the project directory, you can run:
+### inventory edge cases
+Currently if there is a large influx of users that claim all IDs of an item, randomization does not take into account of exhausted inventory. This can be addressed using a couple solutions. Better error checking for balance onchain which is the current solution. Or, ensuring timing is near instant there is a solution for an off-chain cache, that stores in-progress transactions in a cache and balance checking in an off-chain nodejs process.
 
-### `npm start`
+Q: How much is this edge case a problem? 
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Considering equal distribution of 6 items, taking the time for an average transaction of 2 second, there's a ~17% chance if 2 users are racing to get the final item. Therefore, randomization should be retried until transaction successfull. If spatial randomization against a central server is chosen, a centralized cache can be created to that stores transactions <0.5 seconds to avoid this problem.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+## tools used
+- react
+- sequence
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,181 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+import {ethers} from 'ethers'
+import { sequence } from '0xsequence'
+
+import { SequenceIndexerClient } from '@0xsequence/indexer'
+
+const indexer = new SequenceIndexerClient('https://mumbai-indexer.sequence.app')
+
+var startTime: any, endTime: any;
+
+function start() {
+  startTime = new Date();
+};
+
+function end() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime; //in ms
+  var ms = Math.round(timeDiff);
+  return ms
+}
+
+// custom ERC1155 contract
+const contractAddress = '0xE5e38ce8A0bB588D606f7B4A4d92E96ee62576af'
+
+const App = () => {
+  const [signedIn, setSignedIn] = useState(false)
+  const [transition, setTransition] = useState('fade-in')
+  const [reveal, setReveal] = useState(false)
+  const [item, setItem] = useState(false)
+  const [itemImage, setItemImage] = useState('')
+  const [itemId, setItemId] = useState(null)
+  const [itemDescription, setItemDescription] = useState(null)
+
+  sequence.initWallet('mumbai')
+
+  async function checkBalances(){
+    const nftBalances = await indexer.getTokenBalances({
+      contractAddress: contractAddress,
+      accountAddress: contractAddress,
+      includeMetadata: true
+    })
+    return nftBalances.balances.map((balance) => { return {
+      id: Number(balance.tokenID),
+      balance: Number(balance.balance)
+    }})
+  }
+
+  const claim = async (claimedNft: any, claimType: string) => {
+    try {
+      const erc1155Interface = new ethers.utils.Interface([
+        'function claim(uint id, uint _type)'
+      ])
+
+      const wallet = sequence.getWallet()
+
+      const signer = wallet.getSigner()
+
+      const data = erc1155Interface.encodeFunctionData(
+        'claim', [claimedNft, claimType == 'time' ? 0 : claimType == 'hex' ? 1 : 2]
+      )
+
+      const transaction = {
+        to: contractAddress,
+        data: data
+      }
+
+      const txnResponse = await signer.sendTransaction(transaction)
+
+      console.log(txnResponse)
+
+      setItemId(claimedNft)
+      setTransition('fade-out')
+      setTimeout(async () => {
+        setReveal(true)
+        setItem(true)
+        const res = await fetch(`https://bafybeibcge2ljbfhdx54wckh45da7xkbbtxbhnckgup67byj3ntgujesii.ipfs.nftstorage.link/${claimedNft}.json`)
+        const metadata = await res.json()
+        setItemImage(`${metadata.image}`)
+        setItemDescription(metadata.name)
+      }, 4000)
+    } catch(e: any) {
+      console.log('ERROR')
+      if(e.code == '3') {
+        if(claimType == 'time'){
+          timeRandom()
+        } else if(claimType == 'hex') {
+          hexRandom()
+        } else {
+          spaceRandom()
+        }
+      }
+    }
+  }
+
+  const timeRandom = async () => {
+    console.log('time NFT')
+    let balances = await checkBalances()
+    const index = Math.floor(Array.from(Array(Math.floor(Math.random() * (balances).length * 20 + 1)).keys()).reduce((x,y) => x + Math.abs(Math.tan(y)))%(balances).length)
+    claim(balances[index].id, 'time')
+  }
+
+  const hexRandom = async () => {
+    console.log('hex NFT')
+    let balances = await checkBalances()
+    const diff = ethers.BigNumber.from(ethers.utils.hexlify(ethers.utils.randomBytes(20)))
+    claim(balances[Number(diff.toString().substring(0,6)) % balances.length].id, 'hex')
+  }
+
+  const spaceRandom = async () => {
+    console.log('space NFT')
+    start()
+    let balances = await checkBalances()
+    const ttl = end()
+    const claimedNft = balances[ttl % (balances).length].id
+    claim(claimedNft, 'space')
+  }
+
+  const openWallet = async () => {
+    const wallet = sequence.getWallet()
+    wallet.openWallet()
+  }
+
+  const connect = async () => {
+    const wallet = sequence.getWallet()
+
+    const connectWallet = await wallet.connect({
+      app: 'drop',
+      authorize: true,
+      networkId: 8001,
+      settings: {
+        theme: 'light'
+      }
+    })
+
+    if(connectWallet.connected == true) setSignedIn(true)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {/* Loading */}
+      {transition == 'fade-out' && !reveal ? <div className="loading">Loading&#8230;</div> : null}
+      <br/>
+      <br/>
+      {/* Banner */}
+      <img className="center" src="https://sequence.xyz/sequence-wordmark.svg" />
+      <br/>
+      <br/>
+      {/* Pulled Item */}
+      {
+        item ? 
+        (
+          <>
+            <img className="center item-card" src={itemImage} />
+            <p className="item fade-in">You pulled {itemDescription}</p>
+            <button className='connect-button center' onClick={openWallet}>see wallet</button>
+          </>
+        ) : null
+      }
+      {/* randomization shelf */}
+      {
+        signedIn ? 
+          <>
+            <br/>
+            { 
+              ! item ? <p className={`prompt ${transition}`}>claim an NFT, choose your type of randomness</p> : null
+            }
+            <br/>
+            <div className={`container ${transition}`}>
+              <div onClick={timeRandom}><p className="random">time</p></div>
+              <div onClick={hexRandom}><p className="random">hex</p></div>
+              <div onClick={spaceRandom}><p className="random">space</p></div>
+            </div>
+          </>
+        : 
+          <button className="connect-button" onClick={connect}>connect</button>
+      }
     </div>
   );
-}
+};
 
 export default App;
